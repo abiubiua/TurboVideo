bcfda0d831fe3d9cde979a377ffbc8c262d5db9c
luobiao <r.goldchivalry@163.com>
2018-09-08 14:01:23 +0800

    add XCode test project

---
+++ TurboVideo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TurboVideo
+//
+//  Created by luobiao on 2018/9/8.
+//  Copyright © 2018年 luobiao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
